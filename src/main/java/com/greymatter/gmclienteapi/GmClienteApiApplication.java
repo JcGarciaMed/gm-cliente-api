62b5bc056db353131e3c7f0ae732564ef9356bd6
JORGE <jcgarciamed@gmail.com>
2021-09-13 16:30:05 -0500

    initial commit

---
+++ src/main/java/com/greymatter/gmclienteapi/GmClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmclienteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmClienteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmClienteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greymatter/gmclienteapi/GmClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmclienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmClienteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
